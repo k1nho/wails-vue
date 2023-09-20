55014f73f21ef685c8f610818d86bf698677498f
k1nho <kinhong99@gmail.com>
2023-09-20 16:50:48 -0400

    vue vite setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Video from "./components/Video.vue"
+</script>
+
+<template>
+    <div class="bg-red-500 h-screen flex items-center justify-center">
+        <Video></Video>
+    </div>
+</template>
+
+
